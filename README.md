2782a870508e5326d77dc7de49b94443397c2758
lahvdbrand <l.a.h.van.den.brand@student.rug.nl>
2018-05-17 15:54:47 +0200

    Initial project setup.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# RocketScience
+# Rocket Science
+A game where you connect blocks to build large contraptions.
+
+The game is written in **ES6**, **HTML5** and **CSS3**.
+
+## Building
+To generate a packaged file, execute ``npm run pack-dev`` 
+(or ``pack-prod`` for production builds) from the root of the repository. 
+The file will be stored in ``dist``.
+
+## Documentation
+Documentation can be generated using ``npm run doc``.
+
+## Testing
+Unit tests can be performed using ``npm run test``.
+
+## Authors
+Job Talle,
+Luc van den Brand.
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path'),
+      HtmlWebpackPlugin = require('html-webpack-plugin'),
+      ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    target: 'web',
+    entry: './src/app/main.js',
+    output: {
+        filename: 'rocketScience.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                use: 'css-loader'
+            })
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/views/main.html'
+        }),
+        new ExtractTextPlugin('style.css')
+    ]
+};
