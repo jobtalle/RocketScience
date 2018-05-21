a2e8d8855ac86217e5086fa7e79599d1f7f8d19d
Job <jobtalle@hotmail.com>
2018-05-21 14:30:42 +0200

    Refactored

--- src/app/PCB.js
+++ src/app/PCB.js
@@ -15,17 +15,14 @@ export default function PCB() {
 
     const _points = [new Point()];
 
-    let _layer_hull = null;
-    let _layer_parts = null;
-    let _layer_skin = null;
     let _width = 1;
     let _height = 1;
     let _xOrigin = 0.5;
     let _yOrigin = 0.5;
     let _mass = 1;
 
-    const getHullBlock = (x, y) => _points[y][x];
-    const getHullBlockPosition = block => {
+    const getHullPoint = (x, y) => _points[y][x];
+    const getHullPointPosition = block => {
         for(let row = 0; row < _points.length; ++row) {
             const column = _points[row].indexOf(block);
 
@@ -42,61 +39,61 @@ export default function PCB() {
     };
 
     /**
-     * Get the width of this hull in blocks.
+     * Get the width of this PCB in blocks.
      * @returns {Number} The width.
      */
     this.getWidth = () => _width;
 
     /**
-     * Get the height of this hull in blocks.
+     * Get the height of this PCB in blocks.
      * @returns {Number} The height.
      */
     this.getHeight = () => _height;
 
     /**
-     * Get the X origin of this hull in blocks.
+     * Get the X origin of this PCB in blocks.
      * @returns {number} The X origin.
      */
     this.getXOrigin = () => _xOrigin;
 
     /**
-     * Get the Y origin of this hull in blocks.
+     * Get the Y origin of this PCB in blocks.
      * @returns {number} The Y origin.
      */
     this.getYOrigin = () => _yOrigin;
 
     /**
      * Get the mass of this hull.
-     * @returns {number} The total mass of this hull.
+     * @returns {number} The total mass of this PCB and its parts.
      */
     this.getMass = () => _mass;
 
     /**
      * Returns a deep copy of this hull
-     * @returns {Object} A deep copy of this hull.
+     * @returns {Object} A deep copy of this PCB.
      */
     this.copy = () => {
         // TODO
     };
 
     /**
-     * Place a part in the hull, make sure it fits!
-     * @param {Object} block The hull block to place the part in.
+     * Place a part in the PCB, make sure it fits!
+     * @param {Object} point The PCB to place the part in.
      * @param {Object} part A part to place.
      */
-    this.place = (block, part) => {
+    this.place = (point, part) => {
         // TODO: put the part in all hull blocks is covers.
 
-        block.put(part);
+        point.put(part);
     };
 
     /**
-     * Extend the hull
-     * @param {Object} block A hull block to extend from.
+     * Extend the PCB
+     * @param {Object} point A PCB point to extend from.
      * @param {Number} x The X direction to extend to.
      * @param {Number} y The Y direction to extend to.
      */
-    this.extend = (block, x, y) => {
+    this.extend = (point, x, y) => {
         // TODO: Mutate _width and _height here.
     };
 };
