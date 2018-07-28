cb2fbfaf3e3ec7d2ec6e61bbf2f89206bc24905a
jobtalle <jobtalle@hotmail.com>
2018-07-28 10:19:17 +0200

    Created first terrain profile using simplex noise

--- src/app/world/terrain/terrainRugged.js
+++ src/app/world/terrain/terrainRugged.js
@@ -13,19 +13,34 @@ export function TerrainRugged(seed, width) {
     this.getHeights = () => {
         const segments = Math.ceil(width * Terrain.SEGMENTS_PER_METER) + 1;
         const heights = [];
-        const noise = new noisejs.Noise(seed);
         const sampleDirection = Math.PI * 2 * seed;
         const sampleX = Math.cos(sampleDirection);
         const sampleY = Math.sin(sampleDirection);
+        const octaves = makeOctaves(TerrainRugged.OCTAVES, TerrainRugged.FALLOFF);
 
-        for (let i = 0; i < segments; ++i) {
-            const sampleAt = i * Terrain.METERS_PER_SEGMENT;
+        for (let i = 0; i < segments; ++i)
+            heights.push(0);
 
-            heights.push(noise.simplex2(
-                sampleX * sampleAt,
-                sampleY * sampleAt));
+        let scale = TerrainRugged.SCALE;
+
+        for (const octave of octaves) {
+            const noise = new noisejs.Noise(seed++);
+
+            for (let i = 0; i < segments; ++i) {
+                const sampleAt = i * Terrain.METERS_PER_SEGMENT;
+
+                heights[i] += noise.simplex2(
+                    (sampleX * sampleAt) / scale,
+                    (sampleY * sampleAt) / scale) * octave;
+            }
+
+            scale /= TerrainRugged.FALLOFF;
         }
 
         return heights;
     }
 }
+
+TerrainRugged.OCTAVES = 3;
+TerrainRugged.SCALE = 14;
+TerrainRugged.FALLOFF = 2;
