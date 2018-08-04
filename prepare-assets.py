79bca42f7e728e1882a560bdda0f9e27c0cda8d3
jobtalle <jobtalle@hotmail.com>
2018-08-04 16:17:39 +0200

    Asset build script now includes all directories

--- prepare-assets.py
+++ prepare-assets.py
@@ -4,20 +4,29 @@ from subprocess import call
 from distutils.dir_util import copy_tree
 
 def pack_sprites(path):
+    EXTENSION = ".aseprite"
     ATLAS_DATA_PATH  = "src/assets/atlas.json"
     ATLAS_IMAGE_PATH = "dist/atlas.png"     
     NAMING_FORMAT    = "{title}_{frame}"
 
-    command = ("aseprite -b %s/*.aseprite" +
+    command = ("aseprite -b %s" +
                " --data " + ATLAS_DATA_PATH +
                " --sheet-pack --sheet " + ATLAS_IMAGE_PATH +
                " --filename-format " + NAMING_FORMAT)
 
-    print("Packing sprites into '" + ATLAS_DATA_PATH + "' and '" + ATLAS_IMAGE_PATH + "'...");
+    print("Packing sprites into '" + ATLAS_DATA_PATH + "' and '" + ATLAS_IMAGE_PATH + "'...")
+	
+    files = ""
+    
     for (dir_path, dir_names, file_names) in walk(path):
         if (file_names):
+            if (len(files) > 1):
+                files += " "
+            
+            files += dir_path + "/*" + EXTENSION
             print("- '" + dir_path + "'")
-            call(command % dir_path, shell=True)
+    
+    call(command % files, shell=True)
 
 def copy(from_path, to_path):
     print("- '" + from_path + "' into '" + to_path + "'")
