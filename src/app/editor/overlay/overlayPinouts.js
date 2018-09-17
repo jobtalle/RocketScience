f23d3b6f8c98f55f9878922cd9d74564b0d39173
Job <jobtalle@hotmail.com>
2018-09-17 12:34:09 +0200

    Improved pin label direction finding algorithm

--- src/app/editor/overlay/overlayPinouts.js
+++ src/app/editor/overlay/overlayPinouts.js
@@ -35,12 +35,63 @@ export function OverlayPinouts(x, y, configuration) {
 OverlayPinouts.makeLabelDirections = configuration => {
     const directions = [];
     const occupied = [];
-    const isFree = (x, y) => {
-        for (const point of occupied)
-            if (point.x === x && point.y === y)
-                return false;
+    const pins = [];
 
-        return true;
+    const EMPTY = 0;
+    const OCCUPIED = 1;
+    const PIN = 2;
+
+    const getDirection = (x, y) => {
+        let left = EMPTY;
+        let top = EMPTY;
+        let right = EMPTY;
+        let bottom = EMPTY;
+        let direction;
+
+        for (const point of occupied) {
+            if (point.x === x - 1 && point.y === y)
+                left = OCCUPIED;
+            else if (point.x === x && point.y === y - 1)
+                top = OCCUPIED;
+            else if (point.x === x + 1 && point.y === y)
+                right = OCCUPIED;
+            else if (point.x === x && point.y === y + 1)
+                bottom = OCCUPIED;
+        }
+
+        for (const pin of pins) {
+            if (pin.x === x - 1 && pin.y === y)
+                left = PIN;
+            else if (pin.x === x && pin.y === y - 1)
+                top = PIN;
+            else if (pin.x === x + 1 && pin.y === y)
+                right = PIN;
+            else if (pin.x === x && pin.y === y + 1)
+                bottom = PIN;
+        }
+
+        if (top === EMPTY) {
+            if (left === EMPTY && (top === PIN || bottom === PIN))
+                return new Myr.Vector(-1, 0);
+
+            if (right === EMPTY && (top === PIN || bottom === PIN))
+                return new Myr.Vector(1, 0);
+
+            return new Myr.Vector(0, -1);
+        } else if (bottom === EMPTY) {
+            if (left === EMPTY && (top === PIN || bottom === PIN))
+                return new Myr.Vector(-1, 0);
+
+            if (right === EMPTY && (top === PIN || bottom === PIN))
+                return new Myr.Vector(1, 0);
+
+            return new Myr.Vector(0, 1);
+        } else if (left === EMPTY)
+            return new Myr.Vector(-1, 0);
+        else if (right === EMPTY)
+            return new Myr.Vector(1, 0);
+        else
+            return new Myr.Vector(0, -1);
     };
 
     for (const point of configuration.footprint.points)
@@ -52,18 +103,11 @@ OverlayPinouts.makeLabelDirections = configuration => {
     if (configuration.footprint.space) for (const space of configuration.footprint.space)
         occupied.push(space);
 
-    for (const pin of configuration.io) if (pin.type !== Pin.TYPE_STRUCTURAL) {
-        if (isFree(pin.x, pin.y - 1))
-            directions.push(new Myr.Vector(0, -1));
-        else if (isFree(pin.x, pin.y + 1))
-            directions.push(new Myr.Vector(0, 1));
-        else if (isFree(pin.x - 1, pin.y))
-            directions.push(new Myr.Vector(-1, 0));
-        else if (isFree(pin.x + 1, pin.y))
-            directions.push(new Myr.Vector(1, 0));
-        else
-            directions.push(new Myr.Vector(0, -1));
-    }
+    for (const pin of configuration.io)
+        pins.push(pin);
+
+    for (const pin of configuration.io) if (pin.type !== Pin.TYPE_STRUCTURAL)
+        directions.push(getDirection(pin.x, pin.y));
 
     return directions;
 };
