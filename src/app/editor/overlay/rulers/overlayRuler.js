e15d03f007f4b25ca4fe645da40ebe993c24f1e3
jobtalle <jobtalle@hotmail.com>
2018-09-22 14:18:02 +0200

    Styled rulers

--- src/app/editor/overlay/rulers/overlayRuler.js
+++ src/app/editor/overlay/rulers/overlayRuler.js
@@ -11,13 +11,16 @@ export function OverlayRuler(definition) {
 
     const make = () => {
         _element.className = OverlayRuler.CLASS;
-        _element.innerText = definition.length.toString();
         _element.style.width = (definition.length * Pcb.PIXELS_PER_POINT) + "px";
         _element.style.left = (definition.x * Pcb.PIXELS_PER_POINT) + "px";
         _element.style.top = (definition.y * Pcb.PIXELS_PER_POINT) + "px";
 
         if (definition.direction === OverlayRulerDefinition.DIRECTION_UP)
             _element.classList.add(OverlayRuler.CLASS_UP);
+
+        _element.appendChild(OverlayRuler.makeLeft());
+        _element.appendChild(OverlayRuler.makeText(definition.length.toString()));
+        _element.appendChild(OverlayRuler.makeRight());
     };
 
     /**
@@ -29,5 +32,54 @@ export function OverlayRuler(definition) {
     make();
 }
 
+OverlayRuler.makeHBar = () => {
+    const element = document.createElement("div");
+
+    element.className = OverlayRuler.CLASS_HBAR;
+
+    return element;
+};
+
+OverlayRuler.makeVBar = () => {
+    const element = document.createElement("div");
+
+    element.className = OverlayRuler.CLASS_VBAR;
+
+    return element;
+};
+
+OverlayRuler.makeLeft = () => {
+    const element = document.createElement("div");
+
+    element.className = OverlayRuler.CLASS_PADDING;
+    element.appendChild(OverlayRuler.makeVBar());
+    element.appendChild(OverlayRuler.makeHBar());
+
+    return element;
+};
+
+OverlayRuler.makeText = text => {
+    const element = document.createElement("div");
+
+    element.className = OverlayRuler.CLASS_TEXT;
+    element.innerText = text;
+
+    return element;
+};
+
+OverlayRuler.makeRight = () => {
+    const element = document.createElement("div");
+
+    element.className = OverlayRuler.CLASS_PADDING;
+    element.appendChild(OverlayRuler.makeHBar());
+    element.appendChild(OverlayRuler.makeVBar());
+
+    return element;
+};
+
 OverlayRuler.CLASS = "ruler";
 OverlayRuler.CLASS_UP = "up";
+OverlayRuler.CLASS_PADDING = "padding";
+OverlayRuler.CLASS_TEXT = "text";
+OverlayRuler.CLASS_VBAR = "vbar";
+OverlayRuler.CLASS_HBAR = "hbar";
