cd41ffdc99f36b016cc7105be4a33e533c40a0e5
Job <jobtalle@hotmail.com>
2018-10-18 19:37:06 +0200

    Implemented issue #115

--- pack_parts.py
+++ pack_parts.py
@@ -3,17 +3,20 @@ import json
 
 PARTS_DIR = 'src/assets/parts'
 OUT_FILE = 'src/assets/parts.json'
+ORDER_FILE = 'src/assets/parts/order.json'
 
 def merge_two_dicts(copyDict, updateDict):
-    mergeDict = copyDict.copy() 
+    mergeDict = copyDict.copy()
     mergeDict.update(updateDict)
 
     return mergeDict
 
 def get_category_names():
-    categories = next(os.walk(PARTS_DIR))[1]
+    file = open(ORDER_FILE, 'r')
+    data = json.loads(file.read())
+    file.close()
 
-    return categories
+    return data["categoryOrder"]
 
 def get_category_parts(name):
     PART_EXTENSION = 'json'
@@ -24,30 +27,31 @@ def get_category_parts(name):
     for fileName in os.listdir(categoryPartsDir):
         if not fileName.endswith(PART_EXTENSION):
             continue
-        
+
         filePath = os.path.join(categoryPartsDir, fileName)
         print("- '" + filePath + "'")
         with open(filePath) as file:
             partData = json.loads(file.read())
+            file.close()
             categoryParts.append(partData)
 
     return categoryParts
 
-def make_catagory(name):
+def make_category(name):
     PREFIX = 'CATEGORY_'
     LABEL_KEY = 'label'
     PARTS_KEY = 'parts'
-    
+
     category = {}
     category[LABEL_KEY] = PREFIX + name.upper()
     category[PARTS_KEY] = get_category_parts(name)
-    
+
     return category
 
 def merge_parts():
     parts = {}
     for catagoryName in get_category_names():
-        parts[catagoryName] = make_catagory(catagoryName)
+        parts[catagoryName] = make_category(catagoryName)
 
     return parts
 
