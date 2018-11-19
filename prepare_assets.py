0544d20d6d6a2514b73af5342a7e27a8f3df6141
Job <jobtalle@hotmail.com>
2018-11-19 21:07:26 +0100

    Prevented exported language files from only dumping ascii

--- prepare_assets.py
+++ prepare_assets.py
@@ -20,11 +20,12 @@ def copy_langfiles():
 
             json_src_file = open(dir_path + file, encoding="utf-8-sig")
 
-            json_dest_file = open(DIR_DIST + file, "w", encoding="utf-8")
+            json_dest_file = open(DIR_DIST + file, "w", encoding="utf-8-sig")
             json_dest_file.write(json.dumps(
                 json.load(json_src_file),
                 indent=None,
-                separators=(",", ":")))
+                separators=(",", ":"),
+                ensure_ascii=False))
             json_dest_file.close()
 
             json_src_file.close()
