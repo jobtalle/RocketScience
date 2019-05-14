d850ff22a0cc664c3b794532dedb806e9f96626a
YoggYG <ajochembaas@gmail.com>
2019-05-14 14:07:04 +0200

    Readded mission files

--- src/app/gui/editor/pcb/pcbEditor.js
+++ src/app/gui/editor/pcb/pcbEditor.js
@@ -13,7 +13,6 @@ import {UndoStack} from "./undoStack";
 import {Data} from "../../../file/data";
 import {Pcb} from "../../../pcb/pcb";
 import Myr from "myr.js"
-import {downloadBinary} from "../../../utils/downloadBinary";
 
 /**
  * The interactive PCB editor which takes care of sizing & modifying a Pcb.
