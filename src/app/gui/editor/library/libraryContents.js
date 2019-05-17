651e99a6fad5df69375c3358d1e766d677defa99
jobtalle <jobtalle@hotmail.com>
2019-05-17 14:31:08 +0200

    Fixed issue #284

--- src/app/gui/editor/library/libraryContents.js
+++ src/app/gui/editor/library/libraryContents.js
@@ -12,8 +12,22 @@ export function LibraryContents(categories, setPart, info, editable) {
     const _element = document.createElement("div");
     const _categories = [];
 
+    const scroll = delta => {
+        _element.scrollTop += delta;
+    };
+
+    const scrollReset = () => {
+        _element.scrollTop = 0;
+    };
+
     const make = () => {
         _element.id = LibraryContents.ID;
+        _element.addEventListener("wheel", event => {
+            if (event.deltaY < 0)
+                scroll(-LibraryContents.SCROLL_SPEED);
+            else
+                scroll(LibraryContents.SCROLL_SPEED);
+        });
 
         for (const category of categories) {
             const newCategory = new Category(category, setPart, info, editable);
@@ -38,9 +52,12 @@ export function LibraryContents(categories, setPart, info, editable) {
     this.setBudget = (budget, summary) => {
         for (const category of _categories)
             category.setBudget(budget, summary);
+
+        scrollReset();
     };
 
     make();
 }
 
 LibraryContents.ID = "contents";
+LibraryContents.SCROLL_SPEED = 32;
