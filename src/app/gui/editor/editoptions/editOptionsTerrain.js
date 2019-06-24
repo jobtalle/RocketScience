e02d707665274274f2a8b7f72f1d39f0e5c2a0f0
jobtalle <jobtalle@hotmail.com>
2019-06-24 11:26:51 +0200

    Terrain edit options contains a range slider

--- src/app/gui/editor/editoptions/editOptionsTerrain.js
+++ src/app/gui/editor/editoptions/editOptionsTerrain.js
@@ -5,8 +5,21 @@
 export function EditOptionsTerrain() {
     const _element = document.createElement("div");
 
+    const makeRadiusSlider = () => {
+        const slider = document.createElement("input");
+
+        slider.type = "range";
+        slider.min = "1";
+        slider.max = "64";
+        slider.value = "8";
+
+        return slider;
+    };
+
     const build = () => {
         _element.className = EditOptionsTerrain.CLASS;
+
+        _element.appendChild(makeRadiusSlider());
     };
 
     /**
