1bc12c5b17941819ce1fdf8bbb667b18acb4f264
jobtalle <jobtalle@hotmail.com>
2019-07-05 15:29:37 +0200

    Implemented derivative function for water waves

--- src/app/world/water/water.js
+++ src/app/world/water/water.js
@@ -1,9 +1,12 @@
+import {makeOctaves} from "../../utils/octaves";
+
 /**
  * A water plane.
  * @param {Number} amplitude The maximum amplitude of this water surface in pixels.
  * @constructor
  */
 export function Water(amplitude) {
+    const _octaves = makeOctaves(Water.WAVE_FUNCTION_PHASES, Water.WAVE_FUNCTION_FALLOFF);
     let _phase = 0;
 
     /**
@@ -11,7 +14,10 @@ export function Water(amplitude) {
      * @param {Number} timeStep The number of seconds passed since the last update.
      */
     this.update = timeStep => {
-        _phase += timeStep * 2;
+        _phase += timeStep * Water.SPEED;
+
+        if (_phase > Water.WAVE_PHASE_LIMIT)
+            _phase -= Water.WAVE_PHASE_LIMIT;
     };
 
     /**
@@ -33,13 +39,59 @@ export function Water(amplitude) {
     };
 
     /**
+     * Sample the derivative.
+     * @param {Number} x The X location in pixels to sample at.
+     * @returns {Number} The derivative of the waves at this point.
+     */
+    this.sampleDerivative = x => {
+        const left = this.sampleIndex(Math.floor(x / Water.INTERVAL));
+        const f = (x - left * Water.INTERVAL) / Water.INTERVAL;
+
+        return this.sampleDerivativeIndex(left) + (this.sampleDerivativeIndex(left + 1) - this.sampleDerivativeIndex(left)) * f;
+    };
+
+    /**
      * Sample the vertical displacement at an anchor point index.
      * @param {Number} i The index to sample at. Indices are separated by Water.INTERVAL pixels.
      * @returns {Number} The vertical displacement in pixels.
      */
     this.sampleIndex = i => {
-        return Math.cos(i * Water.INTERVAL * 0.07 + _phase) * amplitude * Math.sin(_phase) * 0.3;
+        const index = i * Water.SCALE * Water.INTERVAL;
+        const phase = _phase * Math.PI * 2;
+
+        return (
+            _octaves[0] * Math.cos(index * Water.WAVE_INDEX_SCALE_A - phase * Water.WAVE_SCALE_A) +
+            _octaves[1] * Math.cos(index * Water.WAVE_INDEX_SCALE_B - phase * Water.WAVE_SCALE_B) +
+            _octaves[2] * Math.cos(index * Water.WAVE_INDEX_SCALE_C - phase * Water.WAVE_SCALE_C)
+        ) * amplitude;
+    };
+
+    /**
+     * Sample the slope at an anchor point index.
+     * @param {Number} i The index to sample at. Indices are separated by Water.INTERVAL pixels.
+     * @returns {Number} The derivative of the waves at this point.
+     */
+    this.sampleDerivativeIndex = i => {
+        const index = i * Water.SCALE * Water.INTERVAL;
+        const phase = _phase * Math.PI * 2;
+
+        return (
+            _octaves[0] * Water.WAVE_SCALE_A * Math.sin(index * Water.WAVE_INDEX_SCALE_A - phase * Water.WAVE_SCALE_A) +
+            _octaves[1] * Water.WAVE_SCALE_B * Math.sin(index * Water.WAVE_INDEX_SCALE_B - phase * Water.WAVE_SCALE_B) +
+            _octaves[2] * Water.WAVE_SCALE_C * Math.sin(index * Water.WAVE_INDEX_SCALE_C - phase * Water.WAVE_SCALE_C)
+        ) * -amplitude / Water.INTERVAL;
     };
 }
 
+Water.WAVE_FUNCTION_PHASES = 3;
+Water.WAVE_FUNCTION_FALLOFF = 1.35;
+Water.WAVE_SCALE_A = 1;
+Water.WAVE_SCALE_B = 1.42;
+Water.WAVE_SCALE_C = -0.32;
+Water.WAVE_INDEX_SCALE_A = 1;
+Water.WAVE_INDEX_SCALE_B = 1.31;
+Water.WAVE_INDEX_SCALE_C = 1.42;
+Water.WAVE_PHASE_LIMIT = 100;
 Water.INTERVAL = 16;
+Water.SCALE = 0.08;
+Water.SPEED = 1.3;
