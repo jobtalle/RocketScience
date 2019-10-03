f12cd10e8c8e06373306494941727ed33a04ac37
DanielsWrath <danielhaitink@gmail.com>
2019-10-03 13:54:51 +0200

    Added arrow controllers

---
+++ mods/base/controllerDown/implementation.js
@@ -0,0 +1,31 @@
+/**
+ * A press-able button.
+ * @param {Object} context An object containing the game context, and references to some important utils.
+ * @constructor
+ */
+function ControllerDown(context) {
+    const PIN_INDEX_OUT = 0;
+    const SPRITE_INDEX_BUTTON = 1;
+
+    let _controllerState = null;
+
+    /**
+     * Initialize the state.
+     * @param {Object} body A physics body to apply state to.
+     * @param {ControllerState} controllerState A controller state to read input from.
+     */
+    this.initialize = (body, controllerState) => {
+        _controllerState = controllerState;
+    };
+
+    /**
+     * Update the state.
+     * @param {Array} state A state array to read from and/or write to.
+     */
+    this.tick = state => {
+        state[context.pins[PIN_INDEX_OUT]] = _controllerState.getKeyDown()?1:0;
+        context.renderer.getSprites()[SPRITE_INDEX_BUTTON].setFrame(
+            state[context.pins[PIN_INDEX_OUT]]
+        );
+    };
+}
